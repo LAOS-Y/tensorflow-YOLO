30c5ff52aeb40b154093aab2c69908a5d6701808
LAOS-Y <billyang426@gmail.com>
2018-09-24 03:43:38 +0800

    Add files via upload

--- net/layers.py
+++ net/layers.py
@@ -5,10 +5,10 @@ import net.af as af
 conv = lambda in_layer, filter_num, filter_shape, _strides, _name: tl.layers.Conv2d(
 	in_layer,
 	n_filter = filter_num,
-	act = af.leaky_relu,
 	filter_size = filter_shape,
 	padding = "SAME",
 	strides = _strides,
+	W_init = tf.contrib.layers.xavier_initializer(),
 	name = _name)
 
 dense = lambda in_layer, h, _name: tl.layers.DenseLayer(
@@ -32,3 +32,23 @@ reshape = lambda in_layer, shape, _name: tl.layers.ReshapeLayer(
 	in_layer,
 	shape,
 	name = _name)
+
+batchnorm = lambda in_layer, _name: tl.layers.BatchNormLayer(
+	in_layer,
+	act = af.leaky_relu,
+	name = _name
+)
+
+conv_bn = lambda in_layer, filter_num, filter_shape, _strides, _name: batchnorm(conv(
+	in_layer,
+	filter_num,
+	filter_shape,
+	_strides,
+	_name + 'conv'),
+	_name + 'bn')
+
+transpose = lambda in_layer, dims, _name: tl.layers.TransposeLayer(
+	in_layer,
+	dims,
+	name = _name)
+
